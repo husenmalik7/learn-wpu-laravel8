e4dc31597070f70ae8901788a46d9d1de17047c1
husenmalik7 <husenmalik7@gmail.com>
2021-09-02 13:42:21 +0700

    crud malasngoding

---
+++ database/migrations/2021_09_02_042103_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt'); //readmore
+            $table->text('body');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps(); //(s) method for create update_at and created_at // (no s) is data type
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
+
+
